874dbe4edb2bbcfba50f23be49d5a18482b8df45
David Morrow <dperrymorrow@gmail.com>
2015-12-13 23:15:39 -0800

    initial content

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,14 @@
-//= require_tree .
+//= require jquery/dist/jquery.js
+//= require bootstrap.js
+//= require swipebox/src/js/jquery.swipebox.js
+
+
+$( 'a.gallery' ).swipebox({
+  hideBarsOnMobile: false,
+  hideCloseButtonOnMobile: false,
+  hideBarsDelay : null,
+  afterOpen: function () {
+    debugger;
+  }
+
+});
